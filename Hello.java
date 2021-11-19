eaa0e54d41c743bf974c377a535cfb0190ae240c
Kamol-Paul <s1910176121@ru.ac.bd>
2021-11-19 17:01:59 +0600

    Add multification method

--- Hello.java
+++ Hello.java
@@ -2,9 +2,15 @@ public class Hello{
 	public static void main(String[] args) {
 		int resul = sum(4,5);
 		System.out.println(resul);
+
+		int mulRes = mul(4,3);
+		System.out.println(mulRes);
 	}
 
 	static int sum(int x, int y){
 		return x + y;
+	}
+	static int mul(int x, int y){
+		return x*y;
 	}
 }
