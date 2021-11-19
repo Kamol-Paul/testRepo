dfe42640195f8995c8e912bf2a9eb1867885fa55
Kamol-Paul <s1910176121@ru.ac.bd>
2021-11-19 17:10:16 +0600

    Add subtraction method to master brach

--- Hello.java
+++ Hello.java
@@ -2,9 +2,16 @@ public class Hello{
 	public static void main(String[] args) {
 		int resul = sum(4,5);
 		System.out.println(resul);
+
+		int subRes = sub(10,7);
+		System.out.println(subRes);
 	}
 
 	static int sum(int x, int y){
 		return x + y;
+	}
+
+	static int sub(int a, int b){
+		return a-b;
 	}
 }
